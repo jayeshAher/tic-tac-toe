805e3868aedaf5d36aae84955efe5c2e07d81739
jayeshAher <jayeshvalmikaher@gmail.com>
2024-06-26 08:26:30 +0530

    Use nested maps for rendering the Board grid

--- src/App.js
+++ src/App.js
@@ -50,20 +50,18 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <div>
+        {[...Array(3)].map((_, rowIndex) => (
+          <div key={rowIndex} className="board-row">
+            {[...Array(3)].map((_, colIndex) => {
+              const index = rowIndex * 3 + colIndex;
+              if (index < squares.length) {
+                return <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+              }
+              return null;
+            })}
+          </div>
+        ))}
       </div>
     </>
   );
